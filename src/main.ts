020b649ab1660fafc6a27efa5493b068db59ec13
Romain <romain.jayez@gmail.com>
2022-02-06 17:23:57 +0100

    fix: debug heroku crash

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as helmet from "helmet";
 import { CORS_OPTIONS } from "./constants";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: CORS_OPTIONS });
   app.use(helmet());
   await app.listen(5000);
 }
+process.on("uncaughtException", err => {
+  Logger.warn(err, "LOGGER", false);
+});
+
 bootstrap();
