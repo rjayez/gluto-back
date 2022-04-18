2f3f66cc9a6d2eb8ea4937367806e9d6e544cd8b
Romain <romain.jayez@gmail.com>
2022-04-18 14:18:47 +0200

    feat(wip): users cards collection

--- src/users/schema/user.schema.ts
+++ src/users/schema/user.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
+import { Card } from "../../cards/schema/card.schema";
+import * as mongoose from "mongoose";
 
 export type UserDocument = User & Document;
 
@@ -10,6 +12,9 @@ export class User {
 
   @Prop()
   username: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Card" }] })
+  cards: Card[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,15 @@ import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./schema/user.schema";
+import { Card, CardSchema } from "../cards/schema/card.schema";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Card.name, schema: CardSchema },
+    ]),
+  ],
   providers: [UsersService],
   exports: [UsersService],
 })
