685fd0079f27d86bcc01a373e77741cbd03c5618
Romain <romain.jayez@gmail.com>
2022-05-03 17:31:19 +0200

    test trust proxy config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as helmet from "helmet";
 import { CORS_OPTIONS } from "./constants";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors(CORS_OPTIONS);
   app.use(helmet());
+  app.set("trust proxy", 1);
+
   await app.listen(process.env.PORT);
 }
 
