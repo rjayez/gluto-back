97372382c9dc3ba771200f6d408dc4f8a024f998
Romain <romain.jayez@gmail.com>
2022-07-01 20:05:07 +0200

    fix: word mistake

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -93,9 +93,9 @@ export class BotService implements OnModuleInit {
         const split = message.split("!pelle");
         const messageArg = split[1].trim();
         if (messageArg === "") {
-          await this.chatClient.say(channel, "/me Pelle mets un coup de pelle dans le tchat.");
+          await this.chatClient.say(channel, "/me Pelle met un coup de pelle dans le tchat.");
         } else {
-          await this.chatClient.say(channel, `/me Pelle mets un coup de pelle à ${messageArg}.`);
+          await this.chatClient.say(channel, `/me Pelle met un coup de pelle à ${messageArg}.`);
         }
       }
 
